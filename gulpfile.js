db40e5a568774575fc803a514c0b5bab4917f1d9
Jason Raimondi <jason@raimondi.us>
2015-12-24 01:06:29 -0500

    add templates task to watch

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ requireDir('./gulp');
 gulp.task('default', ['sass', 'browserify', 'templates']);
 
 // Runs all of the above tasks and then waits for files to change
-gulp.task('watch', ['sass', 'browserify'], function() {
+gulp.task('watch', ['sass', 'browserify', 'templates'], function() {
   gulp.watch('./src/assets/sass/**/*.scss', ['sass']);
   gulp.watch('./src/assets/js/**/*.js', ['browserify']);
   gulp.watch('./src/views/**/*.jade', ['templates']);
