57c134fd2abf1a20317162223c80435cc3a0f3c9
Jason Raimondi <jason@raimondi.us>
2015-12-24 00:51:31 -0500

    add jade build task and basic template layout with using flexbox

---
+++ gulp/templates.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var argv = require('yargs').argv;
+var gulp = require('gulp');
+var gulpif = require('gulp-if');
+var jade = require('gulp-jade');
+var minifyHTML = require('gulp-minify-html');
+
+gulp.task('templates', function() {
+
+  var OPTS = {
+    conditionals: true,
+    spare: true
+  };
+
+  gulp.src('./src/views/**/*.jade')
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulpif(argv.production, minifyHTML(OPTS)))
+    .pipe(gulp.dest('./dist/templates'))
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,11 @@ var requireDir = require('require-dir');
 requireDir('./gulp');
 
 // Builds the project
-gulp.task('default', ['sass', 'browserify']);
+gulp.task('default', ['sass', 'browserify', 'templates']);
 
 // Runs all of the above tasks and then waits for files to change
 gulp.task('watch', ['sass', 'browserify'], function() {
   gulp.watch('./src/assets/sass/**/*.scss', ['sass']);
   gulp.watch('./src/assets/js/**/*.js', ['browserify']);
+  gulp.watch('./src/views/**/*.jade', ['templates']);
 });
